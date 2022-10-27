85ec275d8a7bf51ef897c5254d52fee18fe18324
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-10-27 16:22:53 -0500

    fix: remove code field for groups.

--- database/migrations/2022_10_27_040954_create_groups_table.php
+++ database/migrations/2022_10_27_040954_create_groups_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->string('code', length: 24);
             $table->foreignIdFor(App\Models\Subject::class);
             $table->foreignIdFor(App\Models\Professor::class);
             $table->timestamps();
