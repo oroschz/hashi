89b876c3aef73269f36273109f88e497a2a443ad
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-10-27 00:30:27 -0500

    wip: add complex migrations

---
+++ database/migrations/2022_10_27_051630_create_programs_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(App\Models\Program::class);
+            $table->foreignIdFor(App\Models\User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs_users');
+    }
+};
---
+++ database/migrations/2022_10_27_052153_create_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(App\Models\Group::class);
+            $table->foreignIdFor(App\Models\User::class);
+            $table->foreignIdFor(App\Models\Question::class);
+            $table->integer('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
