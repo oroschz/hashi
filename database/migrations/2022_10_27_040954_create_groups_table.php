da7dd9b13c60354b59c62e155e4ae95c9fcfed20
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-10-27 00:14:32 -0500

    wip: add base migrations.

---
+++ database/migrations/2022_10_27_040954_create_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', length: 24);
+            $table->foreignIdFor(App\Models\Subject::class);
+            $table->foreignIdFor(App\Models\Professor::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+};
